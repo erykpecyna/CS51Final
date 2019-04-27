f77879932ba7a39468649c31377bdb536e037c19
erykpecyna <eryk.pecyna@gmail.com>
2019-04-27 00:39:15 -0400

    Primitive implimentation of map generator

--- gameobj.ml
+++ gameobj.ml
@@ -2,35 +2,34 @@ open Graphics ;;
 open Util ;;
 
 (*.............................................................................
-    Graphical Objects 
+  Graphical Objects 
 
-    This class provides a basis for every object in the game. The game objects
-    will necessarily have more functionality, but the drawable object will
-    necessitate their ability to be drawn on the screen
+  This class provides a basis for every object in the game. The game objects
+  will necessarily have more functionality, but the drawable object will
+  necessitate their ability to be drawn on the screen
 .............................................................................*)
 
 class virtual drawable (p : point) =
-    object (this)
-        val pos : point = p
-        method virtual draw : unit
-    end
+  object (this)
+    val pos : point = p
+    method virtual draw : unit
+  end 
 
 class bomb (m : point) =
   object
-    inherit drawable
-    val pos : point = m
-    val radius : int = r
-    val textcolor : color = textcol
-    val linewidth : int = linewidth
-                       
-    method draw =
-      let (x, y) as p = anchor#round in
-      set_line_width linewidth;
-      set_color background;
-      fill_circle x y radius;
-      set_color color;
-      draw_circle x y radius;
-      set_color textcolor;
-      draw_text_centered label p
-  end
+    inherit drawable m
+    (* val radius : int = r *)
+    
+    method draw = ()
+  end 
 
+(*.............................................................................
+  Game Object Types 
+
+  These types differentiate between the types objects that may need to be
+  created
+.............................................................................*)
+type gameobject =
+| Empty
+| Wall
+| Box ;;
