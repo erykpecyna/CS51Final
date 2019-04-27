89179d3d87c811e7fc27f289bbbcf59873d560e7
erykpecyna <eryk.pecyna@gmail.com>
2019-04-26 21:43:01 -0400

    some stuff

--- gamedraw.ml
+++ gamedraw.ml
@@ -1,6 +1,25 @@
+open Graphics
+
 (*.............................................................................
     Drawing Game Objects
 
     These functions are used to access Ocaml's shoddy X11 Graphics support to
     draw the game objects.
  .............................................................................*)
+
+module G = Graphics ;;
+
+(* This function initializes a window and disables automatic drawing to the
+    screen so we can manually draw the buffer to the screen for baby bottom
+    smooth animations*)
+let window_initialize () =
+  G.open_graph "";
+  G.resize_window 800 600;
+  G.auto_synchronize false;
+  G.display_mode false;;
+
+let render_graphics =
+    () ;;
+
+let close_window =
+    ignore (G.read_key ()) ;;
