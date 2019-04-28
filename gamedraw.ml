050a2c1b93b5ecf2ad6c44a0075517b8af2a1f03
erykpecyna <eryk.pecyna@gmail.com>
2019-04-27 23:56:24 -0400

    graphics working

--- gamedraw.ml
+++ gamedraw.ml
@@ -7,19 +7,20 @@ open Graphics
     draw the game objects.
  .............................................................................*)
 
-module G = Graphics ;;
 
 (* This function initializes a window and disables automatic drawing to the
     screen so we can manually draw the buffer to the screen for baby bottom
     smooth animations*)
+
+let initialize_update ()  =
+  flush stdout;
+  raise Exit;;
+
 let window_initialize () =
-  G.open_graph "";
-  G.resize_window 800 600;
-  G.auto_synchronize true;
-  G.display_mode true;;
+  open_graph "";
+  resize_window 800 600;
+  auto_synchronize true;
+  display_mode true;;
 
 let render_graphics =
     () ;;
-
-let close_window =
-    ignore (G.read_key ()) ;;
